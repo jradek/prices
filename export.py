adca25b13987edbc6e92f9109cb8ac56d43c9f68
Jaromir Radek <jradek@yandex.com>
2021-07-09 11:09:51 +0200

    Improved SQL query

--- export.py
+++ export.py
@@ -18,44 +18,50 @@ def get_offers(db: price_db.Database, today=None) -> pd.DataFrame:
     date = today.strftime("%Y-%m-%d")
 
     stmt = f"""
-SELECT
-	d.start,
-	d.end,
-	d.store,
-	i.name,
-	d.amount,
-	d.price_cent,
-	i.unit,
-	i.serving_size,
-	i.category,
-	round(i.serving_size * d.price_cent * 1.0 / d.amount / 100.0, 2) AS "price_per_serving",
-	min_price.store as "min_store",
-	min_price.start as "min_start",
-	min_price.price_per_serving as "min_price_per_serving",
-	measures.num_measures
-FROM discount d
-JOIN item i ON i.id = d.item_id
+SELECT 
+     calc.*,
+     min_price.min_store,
+     min_price.min_start,
+     min_price.min_price_per_serving,
+     calc.price_per_serving < min_price.min_price_per_serving * 1.05 AS "is_deal",
+     measures.num_measures
+FROM (SELECT
+     d."start",
+     d."end",
+     STRFTIME('%s', d.end) - STRFTIME('%s', d.start) <= 2 * 24 * 3600 AS "is_short", 
+     d.store,
+     i.name,
+     i.id,
+     d.amount,
+     d.price_cent,
+     i.unit,
+     i.serving_size,
+     i.category,
+     round(i.serving_size * d.price_cent * 1.0 / d.amount / 100.0, 2) AS "price_per_serving"
+     FROM discount d
+     INNER JOIN item i ON i.id = item_id
+) AS calc
 JOIN (SELECT
-		i.id ,
-		d.store,
-		d.start,
-		round(i.serving_size * d.price_cent * 1.0 / d.amount / 100.0, 2) AS "price_per_serving"
-	FROM discount d
-	JOIN item i on i.id = d.item_id
-	GROUP BY i.id
-	HAVING MIN(round(i.serving_size * d.price_cent * 1.0 / d.amount / 100.0, 2))
+     i.id ,
+     d.store AS "min_store",
+     d."start" AS "min_start",
+     round(i.serving_size * d.price_cent * 1.0 / d.amount / 100.0, 2) AS "min_price_per_serving"
+     FROM discount d
+     JOIN item i on i.id = d.item_id
+     GROUP BY i.id
+     HAVING MIN(round(i.serving_size * d.price_cent * 1.0 / d.amount / 100.0, 2))
 ) AS min_price
 JOIN (SELECT
-		i.id,
-		count(*) as num_measures
-	  FROM discount d
-	  JOIN item i on i.id = d.item_id
-	  GROUP BY i.id
+     i.id,
+     count(*) as num_measures
+     FROM discount d
+     JOIN item i on i.id = d.item_id
+     GROUP BY i.id
 ) AS measures
-WHERE '{date}' <= d.end
-   AND min_price.id = i.id
-   and measures.id = i.id
-ORDER BY d.store, d.start
+WHERE "{date}" < calc."end"
+  AND min_price.id = calc.id
+  AND measures.id = calc.id
+ORDER BY calc.store, calc."start"
     """
 
     def to_date(r):
@@ -63,9 +69,9 @@ ORDER BY d.store, d.start
 
     df = pd.read_sql(stmt, db.get_connection())
     # deal if less than 5% above min price
-    df["is_deal"] = df["price_per_serving"] <= df["min_price_per_serving"] * 1.05
+    # df["is_deal"] = df["price_per_serving"] <= df["min_price_per_serving"] * 1.05
     # short if less than 2 days
-    df["is_short"] = df["end"].map(to_date) - df["start"].map(to_date) <= "2 days"
+    # df["is_short"] = df["end"].map(to_date) - df["start"].map(to_date) <= "2 days"
     print(df)
 
 
