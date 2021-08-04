5aa974744c8635bddb7362d71383f1264f5f489f
Jaromir Radek <jradek@yandex.com>
2021-08-04 09:08:54 +0200

    formatting

--- scripts/current_offers.sh
+++ scripts/current_offers.sh
@@ -14,12 +14,12 @@ query="
 SELECT 
     d.start, 
     d.end, 
-    printf('%15s', d.store), 
+    printf('%12s', d.store), 
     printf('%3d', i.id),
-    printf('%40s', i.name), 
-    printf('%3d', d.amount), 
-    printf('%10s', i.unit), 
-    (d.price_cent * 0.01) || '€' AS euro
+    printf('%45s', i.name), 
+    printf('%4d', d.amount), 
+    printf('%6s', i.unit), 
+    printf('%2.2f€',(d.price_cent * 0.01)) AS price
 FROM discount d
 INNER JOIN item i ON d.item_id = i.id
 WHERE d.end >= \"$now\"
