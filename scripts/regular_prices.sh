81b930520594ff8c47720a0245fbb155aabbb06c
Jaromir Radek <jradek@yandex.com>
2021-08-09 18:55:50 +0200

    formatting

--- scripts/regular_prices.sh
+++ scripts/regular_prices.sh
@@ -6,8 +6,8 @@ SELECT
     printf('%15s', r.store), 
     printf('%3d', i.id), 
     printf('%40s', i.name), 
-    printf('%3d', r.amount), 
-    printf('%10s', i.unit), 
+    printf('%4d', r.amount), 
+    printf('%6s', i.unit), 
     printf(r.price_cent / 100.0) || '€'
 FROM regular r
 INNER JOIN item i ON i.id = r.item_id
