4692e1ebf9bfaeea04dc00a717f2db6736bea9bc
Jaromir Radek <jradek@yandex.com>
2021-08-13 16:43:28 +0200

    implemented per package

--- page/prices.js
+++ page/prices.js
@@ -16,8 +16,8 @@ function generatePrices() {
 
 /**
  * Generate two tabs
- * - Tab1: table with stores and the price per serving
- * - Tab2: table with stores and the price per package
+ * - Tab1: table with stores and the price per package
+ * - Tab2: table with stores and the price per serving
  */
 function get_tabs(
   itemId,
@@ -35,16 +35,18 @@ function get_tabs(
   <div class="col s12">
     <ul class="tabs">
       <li class="tab col s6">
-        <a class="active" href="#${id_left}">Per ${servingSize}${itemUnit} serving</a>
+        <a class="active" href="#${id_left}">Per ${packageSize}${itemUnit} package</a>
       </li>
-      <li class="tab col s6"><a href="#${id_right}">Per ${packageSize}${itemUnit} package</a></li>
+      <li class="tab col s6">
+        <a class="active" href="#${id_right}">Per ${servingSize}${itemUnit} serving</a>
+      </li>
     </ul>
   </div>
   <div id="${id_left}" class="col s12">
-    ${perServingHtml}
+    ${perPackageHtml}
   </div>
   <div id="${id_right}" class="col s12">
-    ${perPackageHtml}
+    ${perServingHtml}
   </div>
 </div>
 `;
@@ -66,20 +68,22 @@ function processRow(data) {
   const categoryClasses = g_categories[category] || "fas fa-question";
 
   const perServingHtml = formatStoresPerServing(itemId);
+  const perPackageHtml = formatStoresPerPackage(data);
 
   const style = "width: 80px; float: left";
 
   // tabs or no tabs
-  const contentHtml = usual_package_size
-    ? get_tabs(
-        itemId,
-        unit,
-        serving_size,
-        usual_package_size,
-        perServingHtml,
-        name
-      )
-    : perServingHtml;
+  const contentHtml =
+    usual_package_size && usual_package_size != serving_size
+      ? get_tabs(
+          itemId,
+          unit,
+          serving_size,
+          usual_package_size,
+          perServingHtml,
+          perPackageHtml
+        )
+      : perServingHtml;
 
   return `
 <li>
@@ -149,7 +153,6 @@ function formatStoresPerServing(itemId) {
 }
 
 function formatPerServingStoreRow(data, overallMinPrice, overallMaxPrice) {
-  // console.log(row);
   // 0: item_id, 1: store, 2: min_price_per_serving, 3: min_date, 4: avg_price_per_serving, 5: max_price_per_serving, 6: max_date, 7: num_measures, 8: regular_price_per_serving, 9: regular_date
 
   const store = data[1];
@@ -187,6 +190,124 @@ function formatPerServingStoreRow(data, overallMinPrice, overallMaxPrice) {
   <td><p>${max_price_per_serving}</p><p style="font-size: x-small">${max_date}</p></td>
   <td>${num_measures}</td>
   <td><p>${regular_price_per_serving}</p><p style="font-size: x-small">${regular_date}</p></td>
+</tr>
+  `;
+}
+
+function formatStoresPerPackage(item) {
+  const itemId = item[0];
+  const servingSize = item[2];
+  const packageSize = item[5];
+  const numServings = packageSize / servingSize;
+
+  const perStoreData = g_pricesPerStore[itemId];
+
+  // no data for this item available
+  if (!perStoreData) {
+    return "";
+  }
+
+  // determine the overall min price per serving for all stores
+  // get value and filter 'null'
+  let minPrices = perStoreData.map((it) => it[2]).filter((x) => x);
+  minPrices = minPrices.sort(numberSorter);
+
+  // min and max price per serving across stores
+  let minPrice = 0;
+  let maxPrice = 1000;
+  if (minPrices.length > 0) {
+    minPrice = minPrices[0];
+    maxPrice = minPrices.pop();
+  }
+
+  const rows = perStoreData.map((store) => {
+    return formatPerPackageStoreRow(store, numServings, minPrice, maxPrice);
+  });
+
+  return `
+<table>
+  <thead>
+    <tr>
+      <th>Store</th>
+      <th>Min</th>
+      <th>Average</th>
+      <th>Max</th>
+      <th>#</th>
+      <th>Regular</th>
+    </tr>
+  </thead>
+  <tbody>
+    ${rows.join(" ")}
+  </tbody>
+</table>`;
+}
+
+function formatPerPackageStoreRow(
+  data,
+  numServings,
+  overallMinPrice,
+  overallMaxPrice
+) {
+  // 0: item_id, 1: store, 2: min_price_per_serving, 3: min_date, 4: avg_price_per_serving, 5: max_price_per_serving, 6: max_date, 7: num_measures, 8: regular_price_per_serving, 9: regular_date
+
+  const store = data[1];
+  const min_price = data[2] ? data[2] : null;
+  // const min_price_per_serving = data[2] ? `${data[2].toFixed(2)}&euro;` : "";
+  const min_price_per_serving = data[2];
+  const min_date = data[3] ? data[3] : "";
+  const avg_price_per_serving = data[4];
+  const max_price_per_serving = data[5];
+  const max_date = data[6] ? data[6] : "";
+  const num_measures = data[7] ? data[7].toFixed(0) : "";
+  const regular_price_per_serving = data[8];
+  const regular_date = data[9] ? data[9] : "";
+
+  var min_price_per_package_s = "";
+  if (min_price_per_serving) {
+    const val = min_price_per_serving * numServings;
+    min_price_per_package_s = `${val.toFixed(2)}&euro;`;
+  }
+
+  var avg_price_per_package_s = "";
+  if (avg_price_per_serving) {
+    const val = avg_price_per_serving * numServings;
+    avg_price_per_package_s = `${val.toFixed(2)}&euro;`;
+  }
+
+  var max_price_per_package_s = "";
+  if (max_price_per_serving) {
+    const val = max_price_per_serving * numServings;
+    max_price_per_package_s = `${val.toFixed(2)}&euro;`;
+  }
+
+  var regular_price_per_package_s = "";
+  if (regular_price_per_serving) {
+    const val = regular_price_per_serving * numServings;
+    regular_price_per_package_s = `${val.toFixed(2)}&euro;`;
+  }
+
+  const storeColors = g_storeToColor[store] || [
+    "#eceff1",
+    "blue-grey lighten-5",
+    "black-text",
+  ];
+
+  // mark best/worst min price per serving
+  let rowStyle = "";
+  if (min_price && min_price < overallMinPrice + 0.01) {
+    rowStyle = "green lighten-5";
+  } else if (min_price && min_price > overallMaxPrice - 0.01) {
+    rowStyle = "red lighten-5";
+  }
+
+  return `
+<tr class="${rowStyle}">
+  <td><span class="my-store-background ${storeColors[1]} ${storeColors[2]}">${store}</span></td>
+  <td><p>${min_price_per_package_s}</p><p style="font-size: x-small">${min_date}</p></td>
+  <td><p>${avg_price_per_package_s}</p><p style="font-size: x-small">&nbsp;</p></td>
+  <td><p>${max_price_per_package_s}</p><p style="font-size: x-small">${max_date}</p></td>
+  <td>${num_measures}</td>
+  <td><p>${regular_price_per_package_s}</p><p style="font-size: x-small">${regular_date}</p></td>
 </tr>
   `;
 }
