52b9065d230e40b87b27c120f2c63e23f7109816
Jaromir Radek <jradek@yandex.com>
2021-08-13 16:49:49 +0200

    removed duplicated code

--- page/prices.js
+++ page/prices.js
@@ -130,8 +130,9 @@ function formatStoresPerServing(itemId) {
     maxPrice = minPrices.pop();
   }
 
+  const numServings = 1.0;
   const rows = perStoreData.map((store) => {
-    return formatPerServingStoreRow(store, minPrice, maxPrice);
+    return formatStoreRow(store, numServings, minPrice, maxPrice);
   });
 
   return `
@@ -150,48 +151,6 @@ function formatStoresPerServing(itemId) {
     ${rows.join(" ")}
   </tbody>
 </table>`;
-}
-
-function formatPerServingStoreRow(data, overallMinPrice, overallMaxPrice) {
-  // 0: item_id, 1: store, 2: min_price_per_serving, 3: min_date, 4: avg_price_per_serving, 5: max_price_per_serving, 6: max_date, 7: num_measures, 8: regular_price_per_serving, 9: regular_date
-
-  const store = data[1];
-  const min_price = data[2] ? data[2] : null;
-  const min_price_per_serving = data[2] ? `${data[2].toFixed(2)}&euro;` : "";
-  const min_date = data[3] ? data[3] : "";
-  const avg_price_per_serving = data[4] ? `${data[4].toFixed(2)}&euro;` : "";
-  const max_price_per_serving = data[5] ? `${data[5].toFixed(2)}&euro;` : "";
-  const max_date = data[6] ? data[6] : "";
-  const num_measures = data[7] ? data[7].toFixed(0) : "";
-  const regular_price_per_serving = data[8]
-    ? `${data[8].toFixed(2)}&euro;`
-    : "";
-  const regular_date = data[9] ? data[9] : "";
-
-  const storeColors = g_storeToColor[store] || [
-    "#eceff1",
-    "blue-grey lighten-5",
-    "black-text",
-  ];
-
-  // mark best/worst min price per serving
-  let rowStyle = "";
-  if (min_price && min_price < overallMinPrice + 0.01) {
-    rowStyle = "green lighten-5";
-  } else if (min_price && min_price > overallMaxPrice - 0.01) {
-    rowStyle = "red lighten-5";
-  }
-
-  return `
-<tr class="${rowStyle}">
-  <td><span class="my-store-background ${storeColors[1]} ${storeColors[2]}">${store}</span></td>
-  <td><p>${min_price_per_serving}</p><p style="font-size: x-small">${min_date}</p></td>
-  <td><p>${avg_price_per_serving}</p><p style="font-size: x-small">&nbsp;</p></td>
-  <td><p>${max_price_per_serving}</p><p style="font-size: x-small">${max_date}</p></td>
-  <td>${num_measures}</td>
-  <td><p>${regular_price_per_serving}</p><p style="font-size: x-small">${regular_date}</p></td>
-</tr>
-  `;
 }
 
 function formatStoresPerPackage(item) {
@@ -221,7 +180,7 @@ function formatStoresPerPackage(item) {
   }
 
   const rows = perStoreData.map((store) => {
-    return formatPerPackageStoreRow(store, numServings, minPrice, maxPrice);
+    return formatStoreRow(store, numServings, minPrice, maxPrice);
   });
 
   return `
@@ -242,12 +201,11 @@ function formatStoresPerPackage(item) {
 </table>`;
 }
 
-function formatPerPackageStoreRow(
-  data,
-  numServings,
-  overallMinPrice,
-  overallMaxPrice
-) {
+/**
+ * If 'numServings = 1', then this is the serving size,
+ * otherwise it would be a package
+ */
+function formatStoreRow(data, numServings, overallMinPrice, overallMaxPrice) {
   // 0: item_id, 1: store, 2: min_price_per_serving, 3: min_date, 4: avg_price_per_serving, 5: max_price_per_serving, 6: max_date, 7: num_measures, 8: regular_price_per_serving, 9: regular_date
 
   const store = data[1];
