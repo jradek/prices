1225766bfb4cc244a724ccd9d5e74d73ffdd90ba
Jaromir Radek <jradek@yandex.com>
2021-08-15 09:36:31 +0200

    reorder columns

--- scripts/regular_prices.sh
+++ scripts/regular_prices.sh
@@ -2,17 +2,17 @@
 
 query="
 SELECT
-    r.date,
-    printf('%15s', r.store),
     printf('%3d', i.id),
     printf('%40s', i.name),
+    printf('%15s', r.store),
+    r.date,
     printf('%4d%-6s', r.amount, i.unit),
     printf('%5.2f€',r.price_cent / 100.0)
 FROM regular r
 INNER JOIN item i ON i.id = r.item_id
 GROUP bY store, name
 HAVING MAX(date)
-ORDER by store, name
+ORDER by name, store
 "
 
 sqlite3 -list -separator " | " tmp/prices.db "${query}"
